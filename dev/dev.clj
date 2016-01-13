9613aef6a18e1740ef50cfa8889d4e991fd33f66
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-13 14:03:43 +0000

    Plug in the ClojureScript REPL into our Clojure REPL

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,5 +1,6 @@
 (ns dev
-  (:require [clojure.test]))
+  (:require [clojure.test]
+            [figwheel-sidecar.repl-api :as fig :refer [start-figwheel! cljs-repl]]))
 
 ;;FIXME: add test directory.
 (defn run-all-tests []
