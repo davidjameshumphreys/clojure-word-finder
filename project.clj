6b79f9f4745620488871d5e2abf2de6a3e7677de
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-14 12:57:44 +0000

    Adding some Clojure server-side libraries & fixing some exclusions.

--- project.clj
+++ project.clj
@@ -5,13 +5,29 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [jarohen/nomad "0.7.2"]
+                 [frankiesardo/pedestal-swagger "0.4.4"]
+                 [io.pedestal/pedestal.service "0.4.1"]
+                 [io.pedestal/pedestal.jetty "0.4.1"]
+
+                 [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.13"]
+                 [org.slf4j/jcl-over-slf4j "1.7.13"]
+                 [org.slf4j/log4j-over-slf4j "1.7.13"]
+                 [org.clojure/tools.logging "0.3.1"]
+
                  [org.clojure/clojurescript "1.7.170"]
                  [devcards "0.2.1"]
                  [sablono "0.4.0"]
+
                  #_[org.omcljs/om "0.9.0"]
                  #_[reagent "0.5.1"]]
-  :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-1"]]
+  :plugins [[lein-cljsbuild "1.1.1"
+             :exclusions [org.clojure/clojure]]
+            [lein-figwheel "0.5.0-1"
+             :exclusions [org.clojure/clojure
+                          org.clojure/tools.reader
+                          ring/ring-core]]]
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "target"]
   :source-paths ["src/clj" "src/cljs"]
@@ -19,7 +35,9 @@
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.cemerick/piggieback "0.2.1"]
-                                  [figwheel-sidecar "0.5.0-1"]]
+                                  [figwheel-sidecar "0.5.0-1"
+                                   :exclusions [org.clojure/clojure
+                                                org.clojure/tools.reader]]]
                    :repl-options {:init-ns          user
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
              }
