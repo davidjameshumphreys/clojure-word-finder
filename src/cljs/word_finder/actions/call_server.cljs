28bc8f3c722989a2c80f094a151daa5b878cbbfb
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-14 21:55:52 +0000

    Added some code to wire-up the server-side calls.  Some devcards demonstrate it.

---
+++ src/cljs/word_finder/actions/call_server.cljs
@@ -0,0 +1,15 @@
+(ns ^:figwheel-load word-finder.actions.call-server
+  (:require [ajax.core :refer [GET]]))
+
+(def server "http://localhost:8080/api/words/")
+
+;;FIXME: the string interpolation can be easily broken.
+(defn find-words [input-string callback]
+  (GET (str server "find" "?find=" input-string)
+      {:handler #(callback %)
+       :error-handler #(println "ERROR" %)}))
+
+(defn find-anagrams [input-string callback]
+  (GET (str server "anagrams" "?find=" input-string)
+      {:handler #(callback %)
+       :error-handler #(println "ERROR!" %)}))
--- src/cljs/word_finder/core.cljs
+++ src/cljs/word_finder/core.cljs
@@ -5,15 +5,15 @@
    [devcards.core :as dc]
    [re-frame.core :as rf]
    [reagent.core :as reagent]
-   [word-finder.components.input :as components])
+   [word-finder.components.input :as components]
+   [word-finder.actions.call-server :as server-side])
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest defcard-rg]]))
 
 (enable-console-print!)
 
 (defcard first-card
-  (sab/html [:div
-             [:h1 "This is your first devcard!"]]))
+  (sab/html [:div "This is your first devcard!"]))
 
 (defcard input-testing
   "Testing our basic input component."
@@ -36,6 +36,22 @@
   (fn [data _]
     (reagent/as-element [components/show-return-values data]))
   input-for-output)
+
+(defcard server-side-testing
+  "### Connecting to the server-side code to take the input and search for it."
+  (fn [data _]
+    ;; this code is bad, callbacks in callbacks!
+    (reagent/as-element [(components/input-text-field (fn [input] (server-side/find-words input (fn [server-data] (reset! data server-data)))))]))
+  {:value nil}
+  {:inspect-data true :history true})
+
+(defcard server-side-anagram
+  "### Connecting to the server-side code to take the input and search for it."
+  (fn [data _]
+    ;; this code is bad, callbacks in callbacks!
+    (reagent/as-element [(components/input-text-field (fn [input] (server-side/find-anagrams input (fn [server-data] (reset! data server-data)))))]))
+  {:value nil}
+  {:inspect-data true :history true})
 
 (defn main []
   ;; conditionally start the app based on wether the #main-app-area
