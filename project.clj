98d036cddb76e1ef026fb9ae2d6051a4624874a4
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-15 08:51:11 +0000

    More :exclusions in the lein configuration, remove some more ambiguity.

--- project.clj
+++ project.clj
@@ -20,11 +20,15 @@
                  [org.clojure/clojurescript "1.7.48" :scope "provided"
                   :exclusions [org.clojure/tools.reader]]
                  [devcards "0.2.1"
-                  :exclusions [org.clojure/tools.reader]]
+                  :exclusions [org.clojure/tools.reader
+                               org.clojure/clojurescript]]
                  [sablono "0.4.0"]
-                 [cljs-ajax "0.5.2"]
-                 [reagent "0.6.0-alpha"]
-                 [re-frame "0.7.0-alpha"]
+                 [cljs-ajax "0.5.2"
+                  :exclusions [org.clojure/clojurescript]]
+                 [reagent "0.6.0-alpha"
+                  :exclusions [org.clojure/clojurescript]]
+                 [re-frame "0.7.0-alpha"
+                  :exclusions [org.clojure/clojurescript]]
                  [org.omcljs/om "0.9.0"]]
   :plugins [[lein-cljsbuild "1.1.2"
              :exclusions [org.clojure/clojure]]
