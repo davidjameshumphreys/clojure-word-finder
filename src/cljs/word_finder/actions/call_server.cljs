c493a7d58878f0c86d1cd5521a04637f6f3abcd9
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-19 09:47:53 +0000

    Fix the cljs server calls to use ajax.core :params
    Instead of doing poor string interpolation, just use the library.

--- src/cljs/word_finder/actions/call_server.cljs
+++ src/cljs/word_finder/actions/call_server.cljs
@@ -3,17 +3,29 @@
 
 (def server "http://localhost:8080/api/words/")
 
-;;FIXME: the string interpolation can be easily broken.
-(defn find-words [input-string callback]
-  (GET (str server "find" "?find=" input-string)
-      {:handler       callback
+(defn find-words
+  "Call the server-side find-words, returning JSON"
+  [input-string callback]
+  (GET (str server "find")
+      {:params        {:find input-string}
+       :format        :json
+       :handler       callback
        :error-handler #(println "ERROR" %)}))
 
-(defn find-anagrams [input-string callback]
-  (GET (str server "anagrams" "?find=" input-string)
-      {:handler       callback
+(defn find-anagrams
+  "Call the server-side find-anangrams, returning JSON"
+  [input-string callback]
+  (GET (str server "anagrams")
+      {:params        {:find input-string}
+       :format        :json
+       :handler       callback
        :error-handler #(println "ERROR!" %)}))
 
-(defn find-sub-anagrams [input-string callback]
-  (GET (str server "sub-anagrams" "?find=" input-string)
-      {:handler callback}))
+(defn find-sub-anagrams
+  "Call the server-side find-sub-anagrams, returning JSON"
+  [input-string callback]
+  (GET (str server "sub-anagrams")
+      {:params        {:find input-string}
+       :format        :json
+       :handler       callback
+       :error-handler #(println "ERROR!" %)}))
